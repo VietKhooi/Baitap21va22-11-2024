6462f8e958f8c77a3452c5ec63ea2effbb11d392
U Nu <91773446+VietKhooi@users.noreply.github.com>
2024-11-23 02:37:25 +0700

    Update main1.js

--- js/main1.js
+++ js/main1.js
@@ -1,46 +1,53 @@
 // Bai 1. Chuyen doi do C sang do F
 
-function nhietdoFsangC(){     //Dinh nghia ham voi ten "nhietdoF", nhan phan tu dau vao la"C"
+function nhietdoCsangF(){     //Dinh nghia ham voi ten "nhietdoF", nhan phan tu dau vao la"C"
     
     let C = document.getElementById("nhapdoC").value;
          
-    C=parseFloat(F);                //parseFloat dung de chuyen doi mot chuoi thanh mot so
+    C=parseFloat(C);                //parseFloat dung de chuyen doi mot chuoi thanh mot so
     let F = (C * 9/5) + 32;       //Cong thuc de chuyen doi tu do C --> do F 
                                 // F=(C * 9/5)+32
-    document.getElementById("KQ").innerText = `${nhietdoC}doC = ${nhietdoF(2)}doF`;
+    document.getElementById("KQ").innerText = `${C} do C = ${F.toFixed(2)} do F`;
     
 }
 
-console.log(nhietdoFsangC);
 
 
 
 //Bai 2: Tao cac ham tinh dien tich
 
 //Tinh dien tich HCN
-function dientichHCN(Dai, Rong){
+function calculateRectangleArea(){
 
-    document.getElementById("HCN");
-    return (Dai*Rong);  //cong thuc dien tich HCN
+    let dai=parseFloat(document.getElementById("chieudai").value);
+    let rong=parseFloat(document.getElementById("chieurong").value);
+
+    let s=dai*rong;     //cong thuc dien tich HCN
+
+    document.getElementById("KQHCN").innerText = `${s.toFixed(2) }`;
+    
 }
-var HCN=dientichHCN(30,10);
-console.log(dientichHCN);
+
 
 //Tinh dien tich TG
-function dientichTG(chieudai, chieucao){
-    document.getElementById("HTG");
-    return (chieudai*chieucao)/2;       //cong thuc dien tich HTG
+function calculateTriangleArea(){
+    let day=parseFloat(document.getElementById("chieuday").value);
+    let cao=parseFloat(document.getElementById("chieucao").value);
+
+    
+    let s = (day*cao)/2;       //cong thuc dien tich HTG
+
+    document.getElementById("KQHTG").innerText = `${s.toFixed(2)}`;
 }
-var HTG=dientichTG(20,5);
-console.log(dientichTG);
+
 
 //Tinh dien tich HV
-function dientichHV(canh){
-    document.getElementById("HV");
-    return (canh*canh);     //cong thuc dien tich HV
+function calculateSquareArea(){
+    let canh=parseFloat(document.getElementById("chieudaicanh").value);
+
+    let s = (canh*canh);     //cong thuc dien tich HV
+    document.getElementById("KQHV").innerText = `${s.toFixed(2)}`;
 }
-var HV=dientichHV(68);
-console.log(dientichHV);
 
 
 
